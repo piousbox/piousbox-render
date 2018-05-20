2bef28438e5d5ec20ed3cdb34277b027c168915b
v p <piousbox@vs-MacBook-Pro.local>
2018-05-20 14:28:35 -0700

    I want Panel.

--- src/index.jsx
+++ src/index.jsx
@@ -1,57 +1,15 @@
-import React from 'react'
 
-class Newsitems extends React.Component {
+class Panel {
   render () {
-    console.log('+++ Newsitems render')
     return (
-      <div>newsitems</div>
+      <div className="dropShadow">
+        <div className="inner">
+          {this.props.children}
+        </div>
+      </div>
     )
   }
 }
 
-class Newsitem extends React.Component {
-  render () {
-    console.log('+++ Newsitem render')
-    return (
-      <div>newsitem</div>
-    )
-  }
-}
+export { Panel }
 
-class NewsitemGallery extends React.Component {
-  render () {
-    console.log('+++ NewsitemGallery render')
-    return (
-      <div>newsitemGallery</div>
-    )
-  }
-}
-
-class NewsitemVideo extends React.Component {
-  render () {
-    console.log('+++ NewsitemVideo render')
-    return (
-      <div>newsitem video</div>
-    )
-  }
-}
-
-class NewsitemReport extends React.Component {
-  render () {
-    console.log('+++ NewsitemReport render')
-    return (
-      <div>newsitem report</div>
-    )
-  }
-}
-
-export default {
-  Newsitems,
-  Newsitem,
-  NewsitemGallery,
-  NewsitemReport,
-  NewsitemVideo
-}
-
-
-        
