42da7e72782996ef3946694d2e6bf8fa1b462c2d
v p <piousbox@vs-MacBook-Pro.local>
2018-05-20 16:32:20 -0700

    bugfixes

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,13 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Panel = exports.Meta = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
@@ -18,130 +19,61 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Newsitems = function (_React$Component) {
-  _inherits(Newsitems, _React$Component);
+var Meta = function (_React$Component) {
+  _inherits(Meta, _React$Component);
 
-  function Newsitems() {
-    _classCallCheck(this, Newsitems);
+  function Meta() {
+    _classCallCheck(this, Meta);
 
-    return _possibleConstructorReturn(this, (Newsitems.__proto__ || Object.getPrototypeOf(Newsitems)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (Meta.__proto__ || Object.getPrototypeOf(Meta)).apply(this, arguments));
   }
 
-  _createClass(Newsitems, [{
-    key: 'render',
+  _createClass(Meta, [{
+    key: "render",
     value: function render() {
-      console.log('+++ Newsitems render');
       return _react2.default.createElement(
-        'div',
+        "div",
         null,
-        'newsitems'
+        _react2.default.createElement(
+          "span",
+          null,
+          "Meta:"
+        ),
+        this.props.children
       );
     }
   }]);
 
-  return Newsitems;
+  return Meta;
 }(_react2.default.Component);
 
-var Newsitem = function (_React$Component2) {
-  _inherits(Newsitem, _React$Component2);
+var Panel = function (_React$Component2) {
+  _inherits(Panel, _React$Component2);
 
-  function Newsitem() {
-    _classCallCheck(this, Newsitem);
+  function Panel() {
+    _classCallCheck(this, Panel);
 
-    return _possibleConstructorReturn(this, (Newsitem.__proto__ || Object.getPrototypeOf(Newsitem)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (Panel.__proto__ || Object.getPrototypeOf(Panel)).apply(this, arguments));
   }
 
-  _createClass(Newsitem, [{
-    key: 'render',
+  _createClass(Panel, [{
+    key: "render",
     value: function render() {
-      console.log('+++ Newsitem render');
+      // console.log('+++ rendering pi Panel:', this.props)
       return _react2.default.createElement(
-        'div',
-        null,
-        'newsitem'
+        "div",
+        { className: "dropShadow" },
+        _react2.default.createElement(
+          "div",
+          { className: "inner" },
+          this.props.children
+        )
       );
     }
   }]);
 
-  return Newsitem;
+  return Panel;
 }(_react2.default.Component);
 
-var NewsitemGallery = function (_React$Component3) {
-  _inherits(NewsitemGallery, _React$Component3);
-
-  function NewsitemGallery() {
-    _classCallCheck(this, NewsitemGallery);
-
-    return _possibleConstructorReturn(this, (NewsitemGallery.__proto__ || Object.getPrototypeOf(NewsitemGallery)).apply(this, arguments));
-  }
-
-  _createClass(NewsitemGallery, [{
-    key: 'render',
-    value: function render() {
-      console.log('+++ NewsitemGallery render');
-      return _react2.default.createElement(
-        'div',
-        null,
-        'newsitemGallery'
-      );
-    }
-  }]);
-
-  return NewsitemGallery;
-}(_react2.default.Component);
-
-var NewsitemVideo = function (_React$Component4) {
-  _inherits(NewsitemVideo, _React$Component4);
-
-  function NewsitemVideo() {
-    _classCallCheck(this, NewsitemVideo);
-
-    return _possibleConstructorReturn(this, (NewsitemVideo.__proto__ || Object.getPrototypeOf(NewsitemVideo)).apply(this, arguments));
-  }
-
-  _createClass(NewsitemVideo, [{
-    key: 'render',
-    value: function render() {
-      console.log('+++ NewsitemVideo render');
-      return _react2.default.createElement(
-        'div',
-        null,
-        'newsitem video'
-      );
-    }
-  }]);
-
-  return NewsitemVideo;
-}(_react2.default.Component);
-
-var NewsitemReport = function (_React$Component5) {
-  _inherits(NewsitemReport, _React$Component5);
-
-  function NewsitemReport() {
-    _classCallCheck(this, NewsitemReport);
-
-    return _possibleConstructorReturn(this, (NewsitemReport.__proto__ || Object.getPrototypeOf(NewsitemReport)).apply(this, arguments));
-  }
-
-  _createClass(NewsitemReport, [{
-    key: 'render',
-    value: function render() {
-      console.log('+++ NewsitemReport render');
-      return _react2.default.createElement(
-        'div',
-        null,
-        'newsitem report'
-      );
-    }
-  }]);
-
-  return NewsitemReport;
-}(_react2.default.Component);
-
-exports.default = {
-  Newsitems: Newsitems,
-  Newsitem: Newsitem,
-  NewsitemGallery: NewsitemGallery,
-  NewsitemReport: NewsitemReport,
-  NewsitemVideo: NewsitemVideo
-};
+exports.Meta = Meta;
+exports.Panel = Panel;
